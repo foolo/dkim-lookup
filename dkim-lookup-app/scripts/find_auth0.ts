11f777650a1cc8e502b592207d457f78645c234f
Olof Andersson <olof.andersson@gmail.com>
2024-03-21 14:39:17 +0100

    update find_auth0.ts

--- dkim-lookup-app/scripts/find_auth0.ts
+++ dkim-lookup-app/scripts/find_auth0.ts
@@ -1,6 +1,16 @@
 import { parse } from 'csv-parse/sync';
 import { readFileSync } from 'fs';
 
+async function checkAuht0Url(domain: string) {
+	//console.log(domain);
+	const domainwithoutTld = domain.split('.').slice(0, -1).join('.');
+	//let url = `https://${domain}/.well-known/openid-configuration
+	let url = `https://${domainwithoutTld}.auth0.com/.well-known/jwks.json`
+	console.log(`domain: ${domain}, url: ${url}`);
+	let response = await fetch(url);
+	let text = await response.text();
+	console.log(`response: ${text.slice(0, 100)}`);
+}
 
 async function main() {
 	let csvFilename = process.argv[2];
@@ -12,7 +22,8 @@ async function main() {
 	const input = readFileSync(csvFilename, 'utf8');
 	const records = parse(input, { columns: false, skip_empty_lines: true });
 	for (let record of records) {
-		console.log(record[domainColumnIndex]);
+		//console.log(record[domainColumnIndex]);
+		await checkAuht0Url(record[domainColumnIndex]);
 	}
 }
 
